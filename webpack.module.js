a7b810c8678308c54dfa294b6d186d3690449397
Gen Ichino <zenoplex@gmail.com>
2015-10-22 22:47:43 +0900

    Removed babel option parameter from webpack

--- webpack.module.js
+++ webpack.module.js
@@ -5,7 +5,7 @@ var loaders = [
     test: /\.scss$/, loader: 'style!css!sass?' +
   'includePaths[]=' + (path.resolve(__dirname, './node_modules'))
   },
-  {test: /\.jsx?$/, loader: 'babel?stage=0', include: path.join(__dirname, './src/js')}
+  {test: /\.jsx?$/, loader: 'babel', include: path.join(__dirname, './src/js')}
 ];
 
 module.exports = {
